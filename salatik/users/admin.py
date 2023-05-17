8c35194955019c2be84d217cc0e18fe076f180cc
Yuri Pilov <zzz---zzz@yandex.ru>
2023-05-17 10:51:48 +0300

    Теперь все сущности можно менять в админке джанги

--- salatik/users/admin.py
+++ salatik/users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Role
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'first_name',
+                    'last_name', 'is_staff', 'is_superuser')
+
+
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+# Регистрация моделей в административной панели
+admin.site.register(User, UserAdmin)
+admin.site.register(Role, RoleAdmin)
