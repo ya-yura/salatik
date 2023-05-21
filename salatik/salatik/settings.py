25e65717fb220bd755a5fdcb58c1435416a68b19
Yuri Pilov <zzz---zzz@yandex.ru>
2023-05-21 20:24:05 +0300

    Добавил шаблон отображения каталога

--- salatik/salatik/settings.py
+++ salatik/salatik/settings.py
@@ -131,8 +131,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
